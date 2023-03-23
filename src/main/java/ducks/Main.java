e682d8fb3ba563c68e5a9e8e5b601807c768ca32
poe-cat <katarzyna.tramea@gmail.com>
2023-03-23 12:04:17 +0100

    translate names and comments from PL to ENG

--- src/main/java/ducks/Main.java
+++ src/main/java/ducks/Main.java
@@ -6,38 +6,45 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        input.useDelimiter("\\s");
-        int n = input.nextInt(); // liczba kaczuszek
-        int m = input.nextInt(); // maksymalna szerokość rzędu
 
-        int[][] kaczuszki = new int[n][2]; // tablica przechowująca wysokość i szerokość kaczuszek
+        input.useDelimiter("\\s");
+
+        int n = input.nextInt(); // number of ducks
+        int m = input.nextInt(); // maximum width of a row
+
+        int[][] ducks = new int[n][2]; // array storing the height and width of the ducks
+
         for (int i = 0; i < n; i++) {
-            kaczuszki[i][0] = input.nextInt(); // wysokość kaczuszki i
-            kaczuszki[i][1] = input.nextInt(); // szerokość kaczuszki s
+            ducks[i][0] = input.nextInt(); // height of duck i
+            ducks[i][1] = input.nextInt(); // width of duck i
         }
 
-        int[][] dp = new int[n][m+1]; // tablica przechowująca maksymalną sumę wysokości dla każdej szerokości rzędu
+        int[][] dp = new int[n][m+1]; // array storing the maximum sum of heights for each row width
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= m; j++) {
-                if (j >= kaczuszki[i][1]) { // jeśli kaczuszka i zmieści się w rzędzie o szerokości j
+                if (j >= ducks[i][1]) { //  if duck i fits in a row with width j
                     if (i == 0) {
-                        dp[i][j] = kaczuszki[i][0]; // pierwsza kaczuszka
+                        dp[i][j] = ducks[i][0]; // first duck
                     } else {
-                        dp[i][j] = Math.max(dp[i-1][j], kaczuszki[i][0] + dp[i-1][j-kaczuszki[i][1]]);
-                        // wybieramy większą wartość spośród dwóch możliwości:
-                        // 1. nie używamy kaczuszki i oraz korzystamy z wyniku z poprzedniego rzędu o szerokości j
-                        // 2. używamy kaczuszki i oraz dodajemy jej wysokość do wyniku z poprzedniego rzędu o szerokości j-kaczuszki[i][1]
+                        dp[i][j] = Math.max(dp[i-1][j], ducks[i][0] + dp[i-1][j-ducks[i][1]]);
+
+                        /* we choose the greater value between two options:
+                        1) we don't use duck i and use the result from the previous row with width j,
+                        2) we use duck i and add its height to the result from the previous row
+                        with width j-ducks[i][1]
+                        */
                     }
                 } else {
                     if (i == 0) {
-                        dp[i][j] = 0; // pierwsza kaczuszka nie zmieści się w rzędzie o szerokości j
+                        dp[i][j] = 0; // the first duck doesn't fit in a row with width j
                     } else {
-                        dp[i][j] = dp[i-1][j]; // nie używamy kaczuszki i
+                        dp[i][j] = dp[i-1][j]; // we don't use duck i
                     }
                 }
             }
         }
 
-        System.out.println(dp[n-1][m]); // wynik to maksymalna suma wysokości dla maksymalnej szerokości rzędu
+        System.out.println(dp[n-1][m]); // the result is the maximum sum of heights for the maximum row width
     }
 }
