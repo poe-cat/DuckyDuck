7c0535da05fefcc15cd3bb3fe99689470c39ebf7
poe-cat <katarzyna.tramea@gmail.com>
2023-03-24 10:02:39 +0100

    separate code into 3 methods: main, readInput, and calculateDp

--- src/main/java/ducks/Main.java
+++ src/main/java/ducks/Main.java
@@ -6,45 +6,71 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         input.useDelimiter("\\s");
 
-        int n = input.nextInt(); // number of ducks
-        int m = input.nextInt(); // maximum width of a row
+        int n = input.nextInt();
+        int m = input.nextInt();
 
-        int[][] ducks = new int[n][2]; // array storing the height and width of the ducks
-
+        int[][] ducks = new int[n][2];
         for (int i = 0; i < n; i++) {
-            ducks[i][0] = input.nextInt(); // height of duck i
-            ducks[i][1] = input.nextInt(); // width of duck i
+            ducks[i][0] = input.nextInt();
+            ducks[i][1] = input.nextInt();
         }
 
-        int[][] dp = new int[n][m+1]; // array storing the maximum sum of heights for each row width
+        int[][] dp = new int[n][m+1];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= m; j++) {
-                if (j >= ducks[i][1]) { //  if duck i fits in a row with width j
+                if (j >= ducks[i][1]) {
                     if (i == 0) {
-                        dp[i][j] = ducks[i][0]; // first duck
+                        dp[i][j] = ducks[i][0];
                     } else {
                         dp[i][j] = Math.max(dp[i-1][j], ducks[i][0] + dp[i-1][j-ducks[i][1]]);
-
-                        /* we choose the greater value between two options:
-                        1) we don't use duck i and use the result from the previous row with width j,
-                        2) we use duck i and add its height to the result from the previous row
-                        with width j-ducks[i][1]
-                        */
                     }
                 } else {
                     if (i == 0) {
-                        dp[i][j] = 0; // the first duck doesn't fit in a row with width j
+                        dp[i][j] = 0;
                     } else {
-                        dp[i][j] = dp[i-1][j]; // we don't use duck i
+                        dp[i][j] = dp[i-1][j];
                     }
                 }
             }
         }
+        System.out.println(dp[n-1][m]);
+    }
 
-        System.out.println(dp[n-1][m]); // the result is the maximum sum of heights for the maximum row width
+    public static int[][] readInput(Scanner input, int n) {
+
+        int[][] ducks = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            ducks[i][0] = input.nextInt();
+            ducks[i][1] = input.nextInt();
+        }
+        return ducks;
+    }
+
+    public static int[][] calculateDp(int[][] ducks, int n, int m) {
+
+        int[][] dp = new int[n][m+1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (j >= ducks[i][1]) {
+                    if (i == 0) {
+                        dp[i][j] = ducks[i][0];
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j], ducks[i][0] + dp[i-1][j-ducks[i][1]]);
+                    }
+                } else {
+                    if (i == 0) {
+                        dp[i][j] = 0;
+                    } else {
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp;
     }
 }
